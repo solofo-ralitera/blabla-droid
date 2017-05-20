cb77186182b3ec30581395c9cd2a30325dd24c94
rasta-popolos <solofo.ralitera@gmail.com>
2017-05-20 22:21:44 +0300

    login

--- app/src/main/java/com/blabla/project/blabla_droid/MainActivity.java
+++ app/src/main/java/com/blabla/project/blabla_droid/MainActivity.java
@@ -2,6 +2,8 @@ package com.blabla.project.blabla_droid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginAction(View view) {
+        String login = ((EditText) findViewById(R.id.login)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+
+        //RequestQueue
+        //RequestQueue queue = Volley.newRequestQueue(this);
+
     }
 }
