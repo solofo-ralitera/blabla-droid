26fb21e2bf07fc29ca2155e26e66e0eece0caa0d
rasta-popolos <solofo.ralitera@gmail.com>
2017-05-20 20:52:07 +0300

    Test listview

--- app/src/main/java/com/blabla/project/blabla_droid/PlayListActivity.java
+++ app/src/main/java/com/blabla/project/blabla_droid/PlayListActivity.java
@@ -2,6 +2,8 @@ package com.blabla.project.blabla_droid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PlayListActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class PlayListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_list);
+
+        String[] playlist = {"List 1", "List 2", "List 3", "List 4"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, playlist);
+        ListView listView = (ListView) findViewById(R.id.listview_playlist);
+        listView.setAdapter(adapter);
+
     }
 }
