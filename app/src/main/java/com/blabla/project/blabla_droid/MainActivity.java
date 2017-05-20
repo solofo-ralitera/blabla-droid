535c1ff28c0bfe9b7fc7c66b8c135d964836d5af
rasta-popolos <solofo.ralitera@gmail.com>
2017-05-20 19:43:06 +0300

    Playlist layout

--- app/src/main/java/com/blabla/project/blabla_droid/MainActivity.java
+++ app/src/main/java/com/blabla/project/blabla_droid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.blabla.project.blabla_droid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when to open playlist */
+    public void openPlayList(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, PlayListActivity.class);
+        startActivity(intent);
+
+
     }
 }
---
+++ app/src/main/java/com/blabla/project/blabla_droid/PlayListActivity.java
@@ -0,0 +1,13 @@
+package com.blabla.project.blabla_droid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_list);
+    }
+}
